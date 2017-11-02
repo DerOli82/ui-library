8b01a18bc82c3f63c506aafcb0e8bf350802ec92
Oliver <DerOli82@alaoli.de>
2017-11-02 18:22:12 +0100

    mark as deprecated

--- src/main/java/de/alaoli/games/minecraft/mods/lib/ui/element/style/Box.java
+++ src/main/java/de/alaoli/games/minecraft/mods/lib/ui/element/style/Box.java
@@ -4,8 +4,12 @@ import de.alaoli.games.minecraft.mods.lib.ui.element.Element;
 
 import java.util.Optional;
 
+/**
+ * @deprecated
+ * @param <T>
+ */
 public interface Box<T extends Element>
 {
-    T setBoxStyle( BoxStyle style );
-    Optional<BoxStyle> getBoxStyle();
+    T setBoxStyle( BoxStyling style );
+    Optional<BoxStyling> getBoxStyle();
 }
