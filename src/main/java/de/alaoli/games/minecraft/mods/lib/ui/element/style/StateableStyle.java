cc9577785af6a77defae1ac89b02faec276b05dc
Oliver <DerOli82@alaoli.de>
2017-11-02 17:10:04 +0100

    Rework stateable style pattern

--- src/main/java/de/alaoli/games/minecraft/mods/lib/ui/element/style/StateableStyle.java
+++ src/main/java/de/alaoli/games/minecraft/mods/lib/ui/element/style/StateableStyle.java
@@ -1,3 +1,21 @@
+/* *************************************************************************************************************
+ * Copyright (c) 2017 DerOli82 <https://github.com/DerOli82>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see:
+ *
+ * https://www.gnu.org/licenses/lgpl-3.0.html
+ ************************************************************************************************************ */
 package de.alaoli.games.minecraft.mods.lib.ui.element.style;
 
 import de.alaoli.games.minecraft.mods.lib.ui.element.state.State;
@@ -6,43 +24,52 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-public class StateableStyle<T extends Style> implements Style
+/**
+ * @author DerOli82 <https://github.com/DerOli82>
+ * @param <T> The type of the child class, required for returning child class in add, remove and set methods
+ * @param <S> The type of the stateable styling class
+ */
+public abstract class StateableStyle<T extends StateableStyle, S extends Style> implements Style
 {
-    /******************************************************************************************
+    /* **************************************************************************************************************
      * Attribute
-     ******************************************************************************************/
+     ************************************************************************************************************** */
 
-    private State fallback = State.NONE;
-    private Map<State, T> states = new HashMap<>();
+    private final Map<State, S> states = new HashMap<>();
 
-    /******************************************************************************************
+    private State currentState = State.NONE;
+
+    /* **************************************************************************************************************
      * Method
-     ******************************************************************************************/
+     ************************************************************************************************************** */
 
-    public Optional<State> getFallback()
+    public StateableStyle( S fallback )
     {
-        return Optional.ofNullable( this.fallback );
+        this.add( State.NONE, fallback );
     }
 
-    public StateableStyle<T> setFallback( State state )
-    {
-        this.fallback = state;
-
-        return this;
-    }
-
-    public StateableStyle<T> add( State state, T style )
+    public T add( State state, S style )
     {
         this.states.put( state, style );
 
-        return this;
+        return (T)this;
     }
 
-    public StateableStyle<T> remove( State state )
+    /**
+     * Note: You cannot remove {@link State#NONE} state, because it must be remain as fallback state.
+     *
+     * @throws IllegalArgumentException, if you try to remove the fallback {@link State#NONE} state.
+     * @param state The state you want to remove
+     * @return      Returns this object
+     *
+     */
+    public T remove( State state ) throws IllegalArgumentException
     {
+        if( state == State.NONE ) { throw new IllegalArgumentException( "Cannot remove 'none' state, because it must be remain as fallback state" );}
+
         this.states.remove( state );
 
-        return this;
+        return (T)this;
     }
 
     public boolean exists( State state )
@@ -50,16 +77,37 @@ public class StateableStyle<T extends Style> implements Style
         return this.states.containsKey( state );
     }
 
-    public Optional<T> get(State state )
+    public Optional<S> get()
     {
-        if( this.states.containsKey( state ) )
+        return this.get( this.currentState );
+    }
+
+    public Optional<S> get( State state )
+    {
+        if( this.states.containsKey( state  ) )
         {
             return Optional.ofNullable( this.states.get( state ) );
         }
+        else if ( this.states.containsKey( State.NONE ) )   //Fallback
+        {
+            return Optional.ofNullable( this.states.get( State.NONE ) );
+        }
         else
         {
-            return Optional.ofNullable( this.states.get( this.fallback ) );
+            return Optional.empty();
         }
+    }
 
+    public T setState( State state )
+    {
+        if( state == null )
+        {
+            this.currentState = State.NONE;
+        }
+        else
+        {
+            this.currentState = state;
+        }
+        return (T)this;
     }
 }
