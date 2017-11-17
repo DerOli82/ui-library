a2c0b7140f3d590950fb3a8a0dc56417655244eb
Oliver <DerOli82@alaoli.de>
2017-11-17 21:13:52 +0100

    Cleanup

--- src/main/java/de/alaoli/games/minecraft/mods/lib/ui/processor/Text.java
+++ src/main/java/de/alaoli/games/minecraft/mods/lib/ui/processor/Text.java
@@ -1,14 +1,35 @@
-package de.alaoli.games.minecraft.mods.lib.ui.element;
+/* *************************************************************************************************************
+ * Copyright (c) 2017 DerOli82 <https://github.com/DerOli82>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see:
+ *
+ * https://www.gnu.org/licenses/lgpl-3.0.html
+ ************************************************************************************************************ */
+package de.alaoli.games.minecraft.mods.lib.ui.processor;
 
-import java.util.Collection;
+import de.alaoli.games.minecraft.mods.lib.ui.element.style.TextStyle;
+
 import java.util.Optional;
 
-public interface Text<T extends Element<T>>
+/**
+ * @author DerOli82 <https://github.com/DerOli82>
+ */
+public interface Text
 {
-    Optional<String> getTextline();
-    Collection<String> getTextlines();
-    T setTextline( String text );
-    T setTextlines( Collection<String> lines );
+    Optional<TextStyle<? extends TextStyle>> getTextStyle();
+    Text setTextStyle( TextStyle<? extends TextStyle> textStyle );
 
-    int countTextlines();
+    Optional<String> getText();
+    Text setText( String text );
 }
